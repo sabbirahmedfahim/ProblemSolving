aaa8100770e2b9afd4bf4cf4b263fb422acde03d
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-03-09 17:50:33 +0600

    file upload

--- Codeforces/A_Chess_For_Three.cpp
+++ Codeforces/A_Chess_For_Three.cpp
@@ -1,23 +1,55 @@
-// solution
 #include <bits/stdc++.h>
 #define nl '\n'
 #define ll long long int
-#define all(v) v.begin(),v.end()
-#define print(v) for(auto data : v) cout << data << " "; cout << nl
+#define all(c) c.begin(),c.end()
+#define print(c) for(auto e : c) cout << e << " "; cout << nl
 using namespace std;
 void solve()
 {
     int x, y, z; cin >> x >> y >> z;
 
-    if((x+y+z)%2 == 1) cout << -1 << nl;
-    else cout << ((x+y+z) - max(0, z - x - y))/2 << nl;
+
+    int res = 0;
+    priority_queue<int> pq;
+    if(x) pq.push(x);
+    if(y) pq.push(y);
+    if(z) pq.push(z);
+        
+    while (!pq.empty())
+    {
+        if(pq.size() == 1) break;
+        int a = pq.top();
+        pq.pop();
+        a--;
+
+        int b = pq.top();
+        pq.pop();
+        b--;
+
+        if(a) pq.push(a);
+        if(b) pq.push(b);
+
+        res++;
+    }
+
+    if(pq.empty()) cout << res << nl;
+    else
+    {
+        int data = pq.top();
+        if(data & 1) cout << -1 << nl;
+        else cout << res << nl;
+    }
 }
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-    int t; cin >> t; 
-    while (t--) solve();
+    int t; cin >> t;
+    for(int tt = 1; tt <= t; tt++)
+    {
+        // cout << "TEST CASE-" << tt << nl;
+        solve();
+    }
 
     return 0;
 }
