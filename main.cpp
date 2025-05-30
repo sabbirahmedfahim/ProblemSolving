606618a86b0950b06b848d9698172edfe87a2ef2
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-05-30 17:27:06 +0600

    first commit from m4

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long
+#define all(c) c.begin(),c.end()s
+#define print(c) for(auto e : c) cout << e << " "; cout << nl
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    cout << "hello worLD" << nl;
+
+    return 0;
+}
