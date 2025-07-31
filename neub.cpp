83fec734a8fb1a70a36dbbe359e46518bcd0e402
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-07-31 19:27:54 +0600

    sorted template

--- neub.cpp
+++ neub.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define nl '\n'
-#define print(c) for(auto e : c) cout << e << " "; cout << nl
+#define endl "\n"
 #define ll long long
 #define ld long double
-#define pie 2*(acos(0.0))
 #define yes cout<<"YES\n"
 #define no cout<<"NO\n"
+#define pie 2*(acos(0.0))
 #define pb push_back
-#define endl "\n"
-#define lcm(a,b) (a*b)/(__gcd<ll>(a,b))
 #define mod 1000000007
+#define lcm(a,b) (a*b)/(__gcd<ll>(a,b))
 #define all(c) c.begin(),c.end()
 #define srt(v) sort(v.begin(),v.end())
 #define rsrt(v) sort(v.rbegin(),v.rend())
-#define ALLAHU_AKBAR ios::sync_with_stdio(false); cin.tie(nullptr);
+#define print(c) for(auto e : c) cout << e << " "; cout << nl
 void solve()
 {
     
