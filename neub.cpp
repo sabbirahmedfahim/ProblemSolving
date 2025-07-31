ad56aa79417828acab77eccc3160df3435742579
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-07-31 19:14:05 +0600

    template

---
+++ neub.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define print(c) for(auto e : c) cout << e << " "; cout << nl
+#define ll long long
+#define ld long double
+#define pie 2*(acos(0.0))
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define pb push_back
+#define endl "\n"
+#define lcm(a,b) (a*b)/(__gcd<ll>(a,b))
+#define mod 1000000007
+#define all(c) c.begin(),c.end()
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
+#define ALLAHU_AKBAR ios::sync_with_stdio(false); cin.tie(nullptr);
+void solve()
+{
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t; cin >> t;
+    for(int tt = 1; tt <= t; tt++)
+    {
+        // cout << "TEST CASE-" << tt << nl;
+        solve();
+    }
+
+    return 0;
+}
