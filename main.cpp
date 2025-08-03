e0fd54f40491127605a90e0e75f5675885d24924
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-08-03 16:25:48 +0600

    file upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,23 @@
-// dsu__kruskal_mst
-struct Edge{
-    int u, v, w; 
-    Edge(int u, int v, int w){
-        this->u = u; this->v = v; this->w = w;
-    }
-};
-bool cmp(Edge a, Edge b){
-    return a.w < b.w;
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long
+#define all(c) c.begin(),c.end()
+#define print(c) for(auto e : c) cout << e << " "; cout << nl
+using namespace std;
+void solve()
+{
+    
 }
-int main(){
-    int n, e; cin >> n >> e;
-    dsu_initialize(n); vector<Edge> edgeList;
-    while (e--){
-        int u, v, w; cin >> u >> v >> w; edgeList.push_back((Edge(u, v, w)));
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t; cin >> t;
+    for(int tt = 1; tt <= t; tt++)
+    {
+        // cout << "TEST CASE-" << tt << nl;
+        solve();
     }
-    sort(edgeList.begin(), edgeList.end(), cmp); 
-    int totalCost = 0; // store costs
-    for (Edge ed : edgeList){
-        int leaderU = dsu_find(ed.u), leaderV = dsu_find(ed.v);
-        if(leaderU == leaderV) continue;
-        else{
-            dsu_union_by_size(ed.u, ed.v); totalCost += ed.w;
-        } 
-    }
-    cout << totalCost;
+
     return 0;
 }
