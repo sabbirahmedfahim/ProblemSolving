ea3992c8da2666d668d6b59e39fc855b32611a1b
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2026-04-29 20:50:24 +0600

    file upload

---
+++ Cute_Substrings.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long
+#define all(c) c.begin(),c.end()
+#define print(c) for(auto e : c) cout << e << " "; cout << nl
+using namespace std;
+void solve()
+{
+    int n; string a; cin >> n >> a;
+
+    int mx = 0, curr = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] == 'w' || a[i] == 'u' || a[i] == 'o') curr++;
+        else 
+        {
+            mx = max(mx, curr); curr = 0;
+        }
+    }
+
+    // cerr << curr << nl;
+    mx = max(mx, curr);
+    
+    cout << mx << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t; cin >> t;
+    for(int tt = 1; tt <= t; tt++)
+    {
+        solve();
+    }
+
+    return 0;
+}
